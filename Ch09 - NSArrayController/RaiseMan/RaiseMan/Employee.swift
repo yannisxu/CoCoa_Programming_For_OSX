36de859fb307e52a78f6c466d336532c4284d122
YannisXu <excellentbright@gmail.com>
2016-06-01 08:19:20 +0800

    finish Ch09 NSArrayController

---
+++ Ch09 - NSArrayController/RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by XuYannis on 16/5/30.
+//  Copyright © 2016年 YannisXu. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
